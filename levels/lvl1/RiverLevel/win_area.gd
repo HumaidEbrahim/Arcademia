f6e12cfcc498c62e8502a9c15666c5c119ddda92
Jan-Paul van den Berg <janpaulvdberg@gmail.com>
2025-09-20 16:40:41 +0200

    Updated River Level

--- levels/lvl1/RiverLevel/win_area.gd
+++ levels/lvl1/RiverLevel/win_area.gd
@@ -1,14 +1,22 @@
-# win_area.gd
 extends Area2D
 
-signal player_won  # Declare the new custom signal
+signal player_won
+
+@onready var main_ui_node = get_node("../main_ui")
+@onready var win_message_label = get_node("../WinMessage")
 
 func _ready() -> void:
-	# Connect the body_entered signal to a function in this script.
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body: Node) -> void:
-	# Check if the entering body is the Player.
 	if body.name == "Player":
-		print("Player has won! Emitting signal.")
-		emit_signal("player_won") # Emit the custom signal to listeners
+		print("Player has won! Emitting signal and showing win message.")
+		emit_signal("player_won")
+		
+		# Stop the command queue by calling the existing clear function.
+		if main_ui_node:
+			main_ui_node._on_clear_pressed()
+			
+		# Show the win message.
+		if win_message_label:
+			win_message_label.visible = true
