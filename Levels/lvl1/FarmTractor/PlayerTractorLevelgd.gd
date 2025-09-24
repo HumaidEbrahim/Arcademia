a3ac283acf4b3c843a21768c56e79a45fb2c0d79
ZeMushroomMan <134008866+ZeMushroomMan@users.noreply.github.com>
2025-09-24 22:25:35 +0200

    Made tractor more tractable

--- Levels/lvl1/FarmTractor/PlayerTractorLevelgd.gd
+++ Levels/lvl1/FarmTractor/PlayerTractorLevelgd.gd
@@ -2,6 +2,10 @@ extends Area2D
 
 @onready var anim = $Sprite2D
 var last_position: Vector2
+@export var left_bound := 100;
+@export var right_bound := 1030;
+@export var bottom_bound := 900;
+@export var top_bound := 500;
 
 func _ready() -> void:
 	last_position = position
@@ -10,8 +14,15 @@ func _process(_delta: float) -> void:
 	if position != last_position:
 		if anim.is_playing() == false:
 			anim.play()
+			if(position < last_position):
+				anim.flip_h = false
+			else:
+				anim.flip_h = true
 	else:
 		if anim.is_playing():
 			anim.pause()
-
+	if(position.x < left_bound || position.x > right_bound):
+		position = last_position
+	if(position.y < top_bound || position.y > bottom_bound ):
+		position = last_position
 	last_position = position
