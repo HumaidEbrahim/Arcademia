0f01eecdbccc616ea7eff4d6041b262d6a37e419
Stefan Paetzold <45475288@mynwu.ac.za>
2025-09-25 09:48:49 +0200

    added a few comments and warning for invalid/short passwords

--- ui/menu/Login/teacher_login.gd
+++ ui/menu/Login/teacher_login.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@onready var label_password: Label = $HeaderPanel/Label
 @onready var line_edit_pin: LineEdit = $HeaderPanel/LineEdit
 @onready var btn_enter: Button = $HeaderPanel/BtnEnter
 @onready var btn_back: Button = $HeaderPanel/BtnBack
@@ -20,13 +21,31 @@ func _ready() -> void:
 	for num in number_buttons:
 		var button: Button = number_buttons[num]
 		button.pressed.connect(add_num_to_pin.bind(num))
-
+	btn_enter.pressed.connect(func(): validate_pin())
 
 func add_num_to_pin(num: int) -> void:
 	if line_edit_pin.text.length() < 4:
 		line_edit_pin.text += str(num)
 
+func is_pin_valid(entered_pin) -> bool:
+	if entered_pin == entered_pin: # TODO fetch pin and check its valid
+		return true
+	else:
+		return false
+
 func validate_pin() -> void:
-	# TODO check the entered pin is valid 
-	print(line_edit_pin.text)
-	pass
+	var entered_pin = line_edit_pin.text
+	
+	if line_edit_pin.text.length() < 4: # Debatable if this is needed but I have it here for now
+		label_password.text = "ENTER FULL PASSWORD"
+		return 
+		
+	if is_pin_valid(entered_pin):
+		pass # TODO go to next screen
+	else:
+		label_password.text = "INCORRECT PASSWORD"
+		
+	line_edit_pin.text = ""
+		
+		
+	
