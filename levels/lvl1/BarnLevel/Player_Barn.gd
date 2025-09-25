8ec998880c7f3ccf5481d969f2d93435240a9b82
HumaidEbrahim <32646111+HumaidEbrahim@users.noreply.github.com>
2025-09-25 21:49:06 +0200

    Instruction box to mainui

--- levels/lvl1/BarnLevel/Player_Barn.gd
+++ levels/lvl1/BarnLevel/Player_Barn.gd
@@ -1,19 +1,21 @@
 extends Area2D
 
+@onready var label = get_parent().get_node("TextBox/Text")
+
 var has_feed = false
 var area = null
 
 func _ready():
 	area_entered.connect(_on_area_entered)
 	area_exited.connect(_on_area_exited)
+	label.text = "Feed the chickens"
 	
 func action_pickup():
 	if area and area.name == "Feed" and not has_feed:
 		has_feed = true
-		print("Pickup")
 		area.queue_free()
 	else:
-		print("go to the bag and pickup")
+		label.text = "Go to the bag and pickup"
 		
 func action_feed():
 	if area and area.name.begins_with("Chicken") and has_feed:
@@ -24,9 +26,9 @@ func action_feed():
 			anim.play("default")
 		check_win()
 	elif area and area.name.begins_with("Chicken") and not has_feed:
-		print("pickup feed first")
+		label.text = "Pickup feed first"
 	else:
-		print("go to chicken to feed it")
+		label.text = "Go to a chicken to feed it"
 
 func _on_area_entered(area2):
 	area = area2
@@ -39,4 +41,4 @@ func check_win():
 	var fed = get_tree().get_nodes_in_group("fed").size()
 	
 	if fed == 2:
-		print("Chickens are all fed")
+		label.text = "Chickens are all fed"
