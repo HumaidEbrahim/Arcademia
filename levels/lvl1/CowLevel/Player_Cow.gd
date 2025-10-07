90b8cd52957bda5de1b157bda2f4e597432d5b9b
Jan-Paul van den Berg <janpaulvdberg@gmail.com>
2025-10-07 13:24:08 +0200

    Added Sounds

--- levels/lvl1/CowLevel/Player_Cow.gd
+++ levels/lvl1/CowLevel/Player_Cow.gd
@@ -3,7 +3,7 @@ extends Area2D
 @onready var label = get_parent().get_node("TextBox/Text")
 @onready var player = $Sprite2D
 @onready var gate = get_parent().get_node("Gate")
-
+@onready var whistle_sound: AudioStreamPlayer2D = get_node("../Player/WhistleSoundPlayer")  # <-- add your whistle sound node
 
 signal levelWon(error:bool)
 
@@ -13,15 +13,23 @@ var chosen_animal = null
 var cow_count:int = 0
 var error:bool = false
 var animals = null
+
 func _ready():
 	area_entered.connect(_on_area_entered)
 
-
 func action_whistle():
 	label.text = "Whistle"
+	
+	# --- Play whistle sound ---
+	if whistle_sound:
+		whistle_sound.play()
+	
 	animals = get_tree().get_nodes_in_group("animals")
+	if animals.size():
+		return
 	chosen_animal = animals.pick_random()
 	print(chosen_animal)
+	
 	var sprite = chosen_animal.get_node("AnimatedSprite2D")
 	sprite.play()
 	
@@ -32,12 +40,11 @@ func action_whistle():
 	else:
 		target_pos = chosen_animal.position + Vector2(50, 2)
 		duration = 1
-		
 	
 	var tween = get_tree().create_tween()
 	tween.tween_property(chosen_animal, "position", target_pos, duration)
 	tween.tween_callback(func():sprite.pause())
-	print("whislte done")
+	print("whistle done")
 
 func action_openGate():
 	if not gate_opened:
@@ -51,7 +58,6 @@ func action_closeGate():
 	gate_opened = false
 	print("closed")
 	
-	
 func _on_area_entered(area2):
 	area2.remove_from_group("animals")
 	print(animals)
@@ -62,11 +68,9 @@ func _on_area_entered(area2):
 		label.text = "cow!"
 		cow_count += 1
 	
-	
 	check_win()
-
 
 func check_win():
 	print(cow_count)
 	if cow_count == 3:
-		emit_signal("levelWon",error)
+		emit_signal("levelWon", error)
