bb8a81d583f10fd70e8fe0412022cd451b6b7f69
Inge Senekal <senekalinge@gmail.com>
2025-10-20 23:29:22 +0200

    Help and maps to MainMenu

--- ui/menu/StudentMenu/main_menu.gd
+++ ui/menu/StudentMenu/main_menu.gd
@@ -3,16 +3,18 @@ extends Control
 
 const STUDENT_SELECTION_PATH := "res://ui/menu/StudentScreens/student_selection.tscn"
 const NEWGAME_SELECTION_PATH := "res://Levels/openworld/FarmMap.tscn"
+const HELP_SELECTION_PATH := "res://ui/menu/StudentMenu/help.tscn"
 
 var new_music = load("res://music/Bongi Eepy/Bongi Eepy (mastered).mp3")
 
 @onready var btn1: Button = $CenterContainer/Menu/BtnNewGame
 @onready var btn2: Button = $CenterContainer/Menu/BtnResume
-@onready var btn3: Button = $CenterContainer/Menu/BtnControls
+@onready var btn3: Button = $CenterContainer/Menu/BtnHelp
 @onready var btn4: Button = $CenterContainer/Menu/BtnBack
 
 @onready var btn_back: BaseButton = $CenterContainer/Menu/BtnBack
 @onready var btn_NewGame: BaseButton = $CenterContainer/Menu/BtnNewGame
+@onready var btn_Help: BaseButton =  $CenterContainer/Menu/BtnHelp
 
 var selected_index: int = 0
 var buttons: Array
@@ -20,6 +22,7 @@ var buttons: Array
 func _ready() -> void:
 	btn_back.pressed.connect(_on_back_pressed)
 	btn_NewGame.pressed.connect(_on_new_game_pressed)
+	btn_Help.pressed.connect(_on_help_pressed)
 	buttons = [btn1, btn3, btn4]
 	buttons[selected_index].grab_focus()
 
@@ -37,3 +40,10 @@ func _on_new_game_pressed() -> void:
 		Pause.toggle_pause()
 		get_tree().change_scene_to_file(NEWGAME_SELECTION_PATH)
 	MusicPlayer.play_stream(new_music, 2.5)
+	
+func _on_help_pressed() -> void:
+	if not Global.GamePaused:
+		get_tree().change_scene_to_file(HELP_SELECTION_PATH)
+	else:
+		Pause.toggle_pause()
+		get_tree().change_scene_to_file(HELP_SELECTION_PATH)	
