b4ca2827d1ba5433e4824f226f9c62a7e393c7dd
HumaidEbrahim <32646111+HumaidEbrahim@users.noreply.github.com>
2025-10-22 19:33:48 +0200

    bomngi chow

--- global.gd
+++ global.gd
@@ -2,7 +2,7 @@ extends Node
 var PersonToEdit :String = "";
 var PersonToGogga:String = "";
 var ActiveStudent:String = "";
-var SelectedCharacter = 0; #0=boy; 1=girl
+var SelectedCharacter = 1; #0=boy; 1=girl
 var populatedExecuteQue:Array = [] #Array for execution panel repopulation
 var GamePaused = false
 var magwinya = 5
